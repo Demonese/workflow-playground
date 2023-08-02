a2c5b302fadf1a2be4bc66757718e4dfa29bd77c
璀境石 <Kuanlan@outlook.com>
2023-08-02 16:36:24 +0800

    Update main.cpp

--- toys/main.cpp
+++ toys/main.cpp
@@ -1,7 +1,11 @@
 #include <cstdio>
+#include <iostream>
+#include "nlohmann/json.hpp"
 
 int main(int, char**)
 {
     std::printf("Hello world!\n");
+    auto j = nlohmann::json::parse(R"({"happy": true, "pi": 3.141})");
+    std::cout << j.dump(4) << std::endl;
     return 0;
 }
